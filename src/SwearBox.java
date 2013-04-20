84de133aebf08fd8b07a3171bb552f5b0b389954
Frank Ellis <silleknarf@gmail.com>
2013-04-20 05:45:14 +0200

    Updated language used

--- src/SwearBox.java
+++ src/SwearBox.java
@@ -21,16 +21,7 @@ public class SwearBox {
 	ArrayList<String[]> swearBox = new ArrayList<String[]>();
 	public SwearBox() {
 		swearBox.add(new String[]{"Fuck", "flip"});
-		swearBox.add(new String[]{"Bitches", "man"});
-		swearBox.add(new String[]{"Bitch", "man"});
-		swearBox.add(new String[]{"Shite", "stuff"});
-		swearBox.add(new String[]{"Shit", "stuff"});
-		swearBox.add(new String[]{"Cunting", "unbelieveable"});
-		swearBox.add(new String[]{"Cunty", "unbelieveable"});
-		swearBox.add(new String[]{"Cunt", "custard pies"});
-		swearBox.add(new String[]{"Scrotum", "bag"});
-		swearBox.add(new String[]{"Wank", "perplexing"});
-		swearBox.add(new String[]{"Mother", "pancake"});
+		// Add your own swear replacements here
 	}
 	public ArrayList<String[]> swears() {
 		return swearBox;
