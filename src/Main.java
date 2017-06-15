1cc0f7216cc27863144cf2d51b7699ee8042d2ee
Frank Ellis <silleknarf@gmail.com>
2017-06-15 13:53:07 +0100

    Create Main.java
    Fixed formatting

--- src/Main.java
+++ src/Main.java
@@ -22,48 +22,44 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 
-public class Main extends JFrame implements ActionListener {
 // Programmer Profanity Preventer
-	/**
-	 * @param args
-	 */
-	
+public class Main extends JFrame implements ActionListener {
+
 	JButton openButton;
 	final JFileChooser fc = new JFileChooser();
 	
 	public Main() {
 		super();
-        //Create and set up the window.
-    JFrame frame = new JFrame("Programmer Profanity Preventer");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLocationRelativeTo(null);
+        	//Create and set up the window.
+		JFrame frame = new JFrame("Programmer Profanity Preventer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 
-    //Create the menu bar.  Make it have a green background.
-    JMenuBar greenMenuBar = new JMenuBar();
-    greenMenuBar.setOpaque(true);
-    greenMenuBar.setBackground(new Color(154, 165, 127));
-    //greenMenuBar.setPreferredSize(new Dimension(500, 500));
+		//Create the menu bar.  Make it have a green background.
+		JMenuBar greenMenuBar = new JMenuBar();
+		greenMenuBar.setOpaque(true);
+		greenMenuBar.setBackground(new Color(154, 165, 127));
+		//greenMenuBar.setPreferredSize(new Dimension(500, 500));
 
-    //Create a yellow label to put in the content pane.
-    JPanel blackLabel = new JPanel();
-    blackLabel.setBackground(new Color(0, 0, 0));
-    
-    openButton = new JButton("Find the fucking swearing...");
-    openButton.setPreferredSize(new Dimension(400, 20));
-    openButton.addActionListener(this);
-    
-    //frame.add(openButton);
-    fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-    
-    //Set the menu bar and add the label to the content pane.
-    frame.setJMenuBar(greenMenuBar);
-    frame.getContentPane().add(openButton, BorderLayout.CENTER);
+		//Create a yellow label to put in the content pane.
+		JPanel blackLabel = new JPanel();
+		blackLabel.setBackground(new Color(0, 0, 0));
 
-    //Display the window.
-    frame.pack();
-    frame.setVisible(true);
+		openButton = new JButton("Find the fucking swearing...");
+		openButton.setPreferredSize(new Dimension(400, 20));
+		openButton.addActionListener(this);
+
+		//frame.add(openButton);
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		//Set the menu bar and add the label to the content pane.
+		frame.setJMenuBar(greenMenuBar);
+		frame.getContentPane().add(openButton, BorderLayout.CENTER);
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
 	}
-	
 	
 	public void replaceAllThatFuckingSwearing(File folder) {
 		Regexes reg = new Regexes();
@@ -86,14 +82,26 @@ public class Main extends JFrame implements ActionListener {
 			System.out.println(file.getName());
 		}
 	}
-    public static void main(String[] args) {
-		
+	
+	public void actionPerformed(ActionEvent e) {
+		//Handle open button action.
+		if (e.getSource() == openButton) {
+			int returnVal = fc.showOpenDialog(Main.this);
+
+		    if (returnVal == JFileChooser.APPROVE_OPTION) {
+			replaceAllThatFuckingSwearing(fc.getSelectedFile());
+			System.exit(0);
+		    } else {
+			System.exit(0);
+		    }
+		}
+	}
+	
+	public static void main(String[] args) {
 		if (args.length == 1) {
-			
 			Regexes reg = new Regexes();
 			String output = reg.replaceAllSwears(args[0]);
 			System.out.println(output);
-		
 		} else {
 			// Schedule a job for the event dispatch thread:
 			// Open the GUI to pick a file
@@ -103,21 +111,5 @@ public class Main extends JFrame implements ActionListener {
 				}
 			}); 
 		}
-    }
-	
-	public void actionPerformed(ActionEvent e) {
-		 
-        //Handle open button action.
-        if (e.getSource() == openButton) {
-        	int returnVal = fc.showOpenDialog(Main.this);
-        	
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                replaceAllThatFuckingSwearing(fc.getSelectedFile());
-            	System.exit(0);
-            } else {
-            	System.exit(0);
-            }
-        }
 	}
-	
 }
